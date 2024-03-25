573bea233d1ca728a7bf7bd78ed50ab9ddfda2e9
manel147 <manelfkih123@gmail.com>
2024-03-25 23:03:21 +0100

    2 commit

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-
+    <View style={styles.container}>
+      <Text>Hello, world!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
